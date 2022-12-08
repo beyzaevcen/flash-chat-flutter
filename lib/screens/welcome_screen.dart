bd8af6de67842980bd62815ec9c874cbfd30b6d5
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-09 00:45:52 +0300

    a

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,7 +9,27 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+
+    controller=AnimationController(
+      duration: Duration(seconds: 1),
+      vsync:this,
+      upperBound: 100.0
+    );
+
+    controller.forward();
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(controller.value);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,9 +42,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag:'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
