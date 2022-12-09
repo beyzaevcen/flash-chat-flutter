20b77c04ce44d4938e9480aa964571aa7766b311
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-10 00:23:33 +0300

    a

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
 
@@ -56,11 +57,11 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     height: 60.0,
                   ),
                 ),
-                Text(
-                  'Flash Chat',
+                TypewriterAnimatedTextKit(
+                 text :['Flash Chat'],
                   //'${controller.value.toInt()}%' => yüzde artarak animasyon
-                  style: TextStyle(
-                    color: Colors.white,
+                  textStyle: TextStyle(
+                    color: Colors.black,
                     fontSize: 40.0,
                     fontWeight: FontWeight.w900,
                   ),
