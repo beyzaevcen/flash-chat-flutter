b680e196cbc1ab9db15d0e204d92ca9813826bf3
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-11 23:12:12 +0300

    a

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/components.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id='registration_id';
@@ -75,25 +76,11 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement registration functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
+            RoundedButton(buttonTitle:"Register",color: Colors.blueAccent,onPressed: (){
+
+            },
             ),
+
           ],
         ),
       ),
