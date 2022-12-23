f5ec436b1ce023154d7fa749553240b2f7eb86af
Beyza Evcen <beyzaevcen37@icloud.com>
2022-12-23 20:10:28 +0300

    a

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -9,6 +9,8 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  String email;
+  String passsword;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +33,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                email=value;
               },
               decoration: kTextFieldDecoration.copyWith(hintText: "Enter your email"),
             ),
@@ -40,7 +42,8 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             TextField(
               onChanged: (value) {
-                //Do something with the user input.
+                passsword=value;
+
               },
               decoration: kTextFieldDecoration.copyWith(hintText: "Enter your password")
             ),
